026e8ad59660c521110ab594801b89a992407e05
yamrire <yamrire@student.1337.ma>
2022-08-25 01:37:57 +0100

    add ft_strlen

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <yamrire@student.1337.ma>          +#+  +:+       +#+        */
+/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 14:03:27 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/21 04:21:08 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/08/24 02:59:08 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include "./../push_swap.h"
 
-int	ft_putchar(char c);
-int	ft_putnbr(long n, int *len);
-int	ft_putstr(char *s);
-int	ft_printf(const char *str, ...);
-int	ft_nbr_base(unsigned long long n, int base, int which, int *len);
+int		ft_putchar(char c);
+int		ft_putnbr(long n, int *len);
+int		ft_putstr(char *s);
+int		ft_printf(const char *str, ...);
+int		ft_nbr_base(unsigned long long n, int base, int which, int *len);
+int		ft_atoi(const char *str);
+char	*ft_strchr(const char *str, int c);
+size_t	ft_strlen(const char *str);
 
 #endif
