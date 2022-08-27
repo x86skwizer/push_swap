327ad52dfb36be632004d13408f908d0d22d6ab2
Yassine Amrire <yamrire@e1r12p10.1337.ma>
2022-08-27 06:54:24 +0100

    add main

--- main.c
+++ main.c
@@ -3,56 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: yamrire <yamrire@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 04:58:40 by yamrire           #+#    #+#             */
-/*   Updated: 2022/08/24 04:19:07 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/08/27 06:53:56 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+
 int	main(int ac, char **av)
 {
 	int	*arr;
-	int high;
 	int	low;
-	int	mid;
-	int	guess;
-	int	item;
 	int	i;
 
     if (ac > 1)
     {
-		arr = malloc(ac * sizeof(int));
+		arr = malloc((ac - 1) * sizeof(int));
 		i = 1;
 		low = 0;
 		while (i < ac)
 		{
+			// check_av_type(ac, av);
 			arr[low] = ft_atoi(av[i]);
 			ft_printf("arr[%d] = %d\n", low, arr[low]);
 			i++;
 			low++;
 		}
-		item = 740;
-		low = 0;
-		high = ac - 1;
-		while (low <= high)
-		{
-			mid = (low + high) / 2;
-			guess = arr[mid];
-			ft_printf("guess = %d\n", guess);
-			if (guess == item)
-			{
-				ft_printf("Found it !\n");
-				return (0);
-			}
-			else if (guess < item)
-				low = mid + 1;
-			else
-				high = mid - 1;
-		}
-			ft_printf("Playing with me ? :(\n");
 	}
 	return (0);
 }
