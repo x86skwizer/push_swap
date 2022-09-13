77aa4500af70693e617c171b06351db7d6bd1f99
yamrire <yamrire@student.1337.ma>
2022-09-13 07:24:15 +0100

    creat struct

--- push_swap.h
+++ push_swap.h
@@ -6,12 +6,20 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 05:05:31 by yamrire           #+#    #+#             */
-/*   Updated: 2022/08/24 02:55:41 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/09/09 04:22:45 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+typedef struct s_data
+{
+	int	*arr;
+	int	*sort_arr;
+	int	size;
+}	t_data;
+
 
 # include "./includes/libftprintf.h"
 # include <stdlib.h>
