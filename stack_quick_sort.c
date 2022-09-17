867d1a4819ce5da4c307eb07eac29b6af8e21498
yamrire <yamrire@student.1337.ma>
2022-09-17 22:14:38 +0100

    rra after sorting

--- stack_quick_sort.c
+++ stack_quick_sort.c
@@ -4,7 +4,7 @@ void sort_stack_a(t_data data, int *top, int end)
 {
     int put_elem;
     int pivot;
-    int push_succ;
+    int rot_succ;
     
     if (!(is_sorted(data.arr, (*top) + 1)))
     {
@@ -14,30 +14,33 @@ void sort_stack_a(t_data data, int *top, int end)
                 sa(data, top);
             return;
         }
-        push_succ = 0;
+        rot_succ = 0; 
         put_elem = ((*top) + 1) / 2;
         pivot = data.sort_arr[(*top) / 2];
         while (put_elem)
         {
             if (data.arr[(*top)] < pivot)
             {
-                pb(top);
-                put_elem--;
-                push_succ++;
+				pb(top);
+				put_elem--;
             }
             else
-                ra(data, top);
-        }
-        //rra
+            {
+            	ra(data, top);
+            	rot_succ++;
+			}
+		  }
+        while (((*top) > 1) && rot_succ)
+		{
+			rra(data, top);
+			rot_succ--;
+		}
+		sort_stack_a(data, top, end);
+		sort_stack_b(data, top, data.size - 1);
     }
-    sort_stack_a(data, top, end);
-    sort_stack_b(data, top, );
 }
 
 void sort_stack_b(t_data data, int *top, int end)
 {
-   while ((*top) + 1 < data.size - 2)
-   {
 
-   }
 }
